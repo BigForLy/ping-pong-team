6d159bced9858001c2c2a39f7afa548c4bd54b6c
BigForLy <oringan@mail.ru>
2022-08-06 12:48:10 +0500

    patch: rename events handler

--- fastapi_app_3/core/events.py
+++ fastapi_app_3/core/events.py
@@ -3,14 +3,14 @@ import asyncio
 from core.kafka import get_kafka_instance
 
 
-async def startup_event():
+async def create_start_app_handler():
     kafka_server = get_kafka_instance()
     await kafka_server.aioproducer.start()
     loop = asyncio.get_event_loop()
     loop.create_task(kafka_server.consume())
 
 
-async def shutdown_event():
+async def create_stop_app_handler():
     kafka_server = get_kafka_instance()
     await kafka_server.aioproducer.stop()
     await kafka_server.aioconsumer.stop()
--- fastapi_app_3/main.py
+++ fastapi_app_3/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from core.config import get_settings
 from core.const import SHUTDOWN_EVENT, STARTUP_EVENT
-from core.events import shutdown_event, startup_event
+from core.events import create_stop_app_handler, create_start_app_handler
 from routes import router as router_api
 
 
@@ -11,8 +11,8 @@ def get_application() -> FastAPI:
 
     application.include_router(router_api, prefix=settings.router_prefix)
 
-    application.add_event_handler(STARTUP_EVENT, startup_event)
-    application.add_event_handler(SHUTDOWN_EVENT, shutdown_event)
+    application.add_event_handler(STARTUP_EVENT, create_start_app_handler)
+    application.add_event_handler(SHUTDOWN_EVENT, create_stop_app_handler)
 
     return application
 
